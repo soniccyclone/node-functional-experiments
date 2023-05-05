60ddd548447e06fd2b3d550be03bea177923aba2
soniccyclone <5423744-soniccyclone@users.noreply.replit.com>
2023-05-05 22:28:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+comp = (...f) =>
+  (...args) => {
+    innerComp = (g) =>
+      (g.length === 1
+       ? g[0](...args)
+       : g[0](innerComp(g.slice(1))));
+    return innerComp(f);
+  }
+increment = (n) => n+1;
+plus = (...n) => n.reduce((acc, val) => acc + val, 0);
+
+console.log(comp(increment, plus)(1, 2, 3));
